ebeaaf5b2ea0469dc5b176696c47f416fbe4e9d0
yeongwoojang <sbtmxhs@gmail.com>
2021-08-09 00:22:28 +0900

    Initial commit

---
+++ app/src/main/java/com/sayeong/prac_mvp_pattern/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ywjang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sayeong.prac_mvp_pattern.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
